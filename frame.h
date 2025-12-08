2c9d067e49a8616e87d4e00294032a9f0eb4356e
nneagle <nicholasaneagle@gmail.com>
2025-12-08 13:47:22 -0500

    comments

--- frame.h
+++ frame.h
@@ -17,15 +17,31 @@ public:
     Frame(void);
     ~Frame(void);
 
+    // returns the PID associated with the frame
     string getId(void) const;
+
+    // returns the frequency assocaited with the frame
     int getFreq(void) const;
+
+    // returns the value of the valid bit associated with the frame
     bool getValid(void) const;
+
+    // returns the page number associated with the frame
     int getPageNum(void) const;
 
+    // sets the PID associated with the frame
     void setId(string inputId);
+
+    // sets the frequency associated with the frame
     void setFrequency(int inputFreq);
+
+    // increments the frequency assocated with the frame by 1
     void incFrequency(void);
+
+    // toggles the valid bit associted with the frame
     void toggleValid(void);
+
+    // sets the page number associated with the frame
     void setPageNum(int pgNum);
 
 private:
